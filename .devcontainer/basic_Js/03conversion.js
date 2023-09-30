179d704057a620093b0a0aeadcbbd9df5a38a3b1
Rajesh Prajapati <114566351+prajapatirajesh2002@users.noreply.github.com>
2023-09-30 19:35:19 +0000

    third

---
+++ .devcontainer/basic_Js/03conversion.js
@@ -0,0 +1,8 @@
+let score = "33";
+console.log(score);
+let name1 = "rajesh";
+
+let  valueInNumber = Number(score);
+let convert = Number(name1);
+console.log(convert);
+console.log("this is converted into number "+Number(null));
