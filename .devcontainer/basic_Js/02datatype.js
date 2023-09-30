163ff4c2e3395db522bc5032edd9cf3db3a121a0
Rajesh Prajapati <114566351+prajapatirajesh2002@users.noreply.github.com>
2023-09-30 19:26:19 +0000

    hlw

---
+++ .devcontainer/basic_Js/02datatype.js
@@ -0,0 +1,15 @@
+"use strick";// treat all js code as newer version
+// alert(3+3) // use we are using nodejs , not brower
+/*
+data type name :
+number => 2 to power 53
+bigint
+string => ""
+boolean = >true/ false;
+null = standalone balue 
+undefined 
+symbol= > unique
+object 
+*/
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ .devcontainer/basic_Js/veriable.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountPassword = "12345"
+var accountEmain = "prajapatirajesh124@gmail.com";
+/*
+prefer  not to use of var because of issue of scope and funtional scope
+*/
+console.log(accountEmain);
+useName = "Rajesh@698";
+
+// this is second way of print the all variables;
+console.table([accountId,accountPassword,accountEmain,useName])
